a598416c90de1b21bfbd3b4ff799ebac27805c9f
yuuy <weiyushuai666@gmail.com>
2021-05-05 20:53:27 +0800

    CopyOnWriteArrayList add lock only on method add

--- src/com/yuuy/juc/CollectionNotSafe.java
+++ src/com/yuuy/juc/CollectionNotSafe.java
@@ -1,20 +1,20 @@
 package com.yuuy.juc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.Vector;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * 1. ArrayList is thread unsafe.
  *  - not in sequence when adding
  *  - ConcurrentModifiedException
- *      - Solution: 1. Vector : 读性能受限
+ *      - Solution: 1. Vector  : 读性能受限
+ *                  2. Collection.synchronizedList: 读性能依旧受限
+ *                  3. CopyOnWriteArrayList : 只对写加锁，读性能不受影响
  */
 public class CollectionNotSafe {
 
     public static void main(String[] args) {
-        List<String> list = new Vector<>();
+        List<String> list = new CopyOnWriteArrayList<>();
 
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
