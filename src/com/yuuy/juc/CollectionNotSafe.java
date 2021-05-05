1895fc3527b7faefe813b01af913c175600325d3
yuuy <weiyushuai666@gmail.com>
2021-05-05 20:41:21 +0800

    Vector adding concurrently

--- src/com/yuuy/juc/CollectionNotSafe.java
+++ src/com/yuuy/juc/CollectionNotSafe.java
@@ -2,16 +2,26 @@ package com.yuuy.juc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.Vector;
 
+/**
+ * 1. ArrayList is thread unsafe.
+ *  - not in sequence when adding
+ *  - ConcurrentModifiedException
+ *      - Solution: 1. Vector : 读性能受限
+ */
 public class CollectionNotSafe {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<String> list = new Vector<>();
 
-        list.add("A");
-        list.add("B");
-        list.add("C");
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
 
-        list.forEach(System.out::println);
     }
 }
