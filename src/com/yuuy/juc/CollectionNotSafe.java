a4160505eb4c23c8ad8b34ec8d245a0549cca749
yuuy <weiyushuai666@gmail.com>
2021-05-05 21:10:18 +0800

    Collections.synchronizedSet add lock on methods of Set

--- src/com/yuuy/juc/CollectionNotSafe.java
+++ src/com/yuuy/juc/CollectionNotSafe.java
@@ -1,25 +1,23 @@
 package com.yuuy.juc;
 
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * 1. ArrayList is thread unsafe.
+ * 1. HashSet is thread unsafe.
  *  - not in sequence when adding
  *  - ConcurrentModifiedException
- *      - Solution: 1. Vector  : 读性能受限
- *                  2. Collection.synchronizedList: 读性能依旧受限
- *                  3. CopyOnWriteArrayList : 只对写加锁，读性能不受影响
+ *      - Solution: 1. Collections.synchronizedSet: 读性能依旧受限
+ *                  2. CopyOnWriteSet : 只对写加锁，读性能不受影响
  */
 public class CollectionNotSafe {
 
     public static void main(String[] args) {
-        List<String> list = new CopyOnWriteArrayList<>();
+        Set<String> set = Collections.synchronizedSet(new HashSet<>());
 
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
-                list.add(UUID.randomUUID().toString().substring(0, 8));
-                System.out.println(list);
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
             }, String.valueOf(i)).start();
         }
 
