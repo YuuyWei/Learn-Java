4e6dd95e49eac1b91c0acf2b1f63ff867bbe3610
yuuy <weiyushuai666@gmail.com>
2021-05-05 20:01:18 +0800

    Eight Lock: normal method

--- src/com/yuuy/juc/EightLock.java
+++ src/com/yuuy/juc/EightLock.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 class Screen {
 
     synchronized void showFlower() throws InterruptedException{
+        TimeUnit.SECONDS.sleep(4);
         System.out.println("----showing flower");
     }
 
@@ -12,12 +13,22 @@ class Screen {
         System.out.println("----showing people");
     }
 
+    void welcome() throws InterruptedException{
+        System.out.println("----hello");
+    }
+
 }
 
 /**
  * 线程8锁，线程A先启动，线程B后启动。先显示花还是人?
  * 1. 标准访问：花
- * 2. 花暂停4秒：
+ * 2. 花暂停4秒：花
+ * 3. 增加一个普通方法welcome: 先显示hello
+ * 4. 两个Screen对象
+ * 5. 两个静态同步方法，两个Screen对象
+ * 6. 两个静态同步方法，一个Screen对象
+ * 7. 一个普通同步方法，一个静态同步方法，两个Screen对象
+ * 8. 一个普通同步方法，一个静态同步方法，一个Screen对象
  */
 public class EightLock {
 
@@ -38,7 +49,8 @@ public class EightLock {
         new Thread(() -> {
 
             try {
-                screen.showPeople();
+//                screen.showPeople();
+                screen.welcome();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
