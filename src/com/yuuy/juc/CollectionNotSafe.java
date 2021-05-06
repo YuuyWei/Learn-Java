bdadfb9110ee2c5b7d3245da7ef93025b47fdbd5
yuuy <weiyushuai666@gmail.com>
2021-05-06 09:30:34 +0800

    ConcurrentHashMap and synchronizedMap

--- src/com/yuuy/juc/CollectionNotSafe.java
+++ src/com/yuuy/juc/CollectionNotSafe.java
@@ -1,6 +1,8 @@
 package com.yuuy.juc;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
@@ -13,6 +15,28 @@ import java.util.concurrent.CopyOnWriteArraySet;
 public class CollectionNotSafe {
 
     public static void main(String[] args) {
+        Map<String, String> map = new ConcurrentHashMap<>();
+//        Map<String, String> map = Collections.synchronizedMap(new HashMap<>());
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0,8));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+    public static void listNotSafe() {
+        List<String> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+    public static void setNotSafe() {
         Set<String> set = new CopyOnWriteArraySet<>();
 
         for (int i = 0; i < 100; i++) {
@@ -21,6 +45,5 @@ public class CollectionNotSafe {
                 System.out.println(set);
             }, String.valueOf(i)).start();
         }
-
     }
 }
